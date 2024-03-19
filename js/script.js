5f925f902201e5486847a0dd916e5f5e7ccdc3a9
BarnaSanyi <146037957+BarnaSanyi@users.noreply.github.com>
2024-03-19 02:27:57 +0100

    FElcsúszós header 50%-os
    Hihi
    :)

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".menu");
-const header = document.querySelector(header);
 
 hamburger.addEventListener("click", () =>{
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+    hamburger.classList.toggle("active")
+    navMenu.classList.toggle("active")
 })
 
-window.addEventListener("scroll", () =>{
-    
-    
-    console.log(document.documentElement.scrollHeight)
+const body = document.querySelector("#pageHeader");
+let lastScroll = 0;
+
+window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset
+
+    if(currentScroll <= 0){
+        body.classList.remove("scrollUp")
+    }
+        
+    if(currentScroll > lastScroll && !body.classList.contains("scrollDown")){
+        body.classList.remove("scrollUp")
+        body.classList.add("scrollDown")
+    }
+
+    if(currentScroll < lastScroll && body.classList.contains("scrollDown")){
+        body.classList.remove("scrollDown")
+        body.classList.add("scrollUp")
+    }
+
+    lastScroll = currentScroll;
 })
