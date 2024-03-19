e42b8b2d03408494756ba973aad70adc845bc029
BarnaSanyi <146037957+BarnaSanyi@users.noreply.github.com>
2024-03-19 10:25:59 +0100

    Header done
    Már csak a kérdések szekció és kész a főoldal :)

--- js/script.js
+++ js/script.js
@@ -1,29 +1,34 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".menu");
+const body = document.querySelector("#pageHeader");
+let lastScroll = 0;
 
 hamburger.addEventListener("click", () =>{
     hamburger.classList.toggle("active")
     navMenu.classList.toggle("active")
+    body.classList.toggle("active")
 })
-
-const body = document.querySelector("#pageHeader");
-let lastScroll = 0;
 
 window.addEventListener('scroll', () => {
     const currentScroll = window.pageYOffset
-
-    if(currentScroll <= 0){
-        body.classList.remove("scrollUp")
-    }
-        
-    if(currentScroll > lastScroll && !body.classList.contains("scrollDown")){
-        body.classList.remove("scrollUp")
+ 
+    if(currentScroll > lastScroll && !body.classList.contains("scrollDown") && !body.classList.contains("active")){
         body.classList.add("scrollDown")
+        
     }
 
-    if(currentScroll < lastScroll && body.classList.contains("scrollDown")){
+    if(currentScroll < lastScroll && body.classList.contains("scrollDown") && !body.classList.contains("active")){
         body.classList.remove("scrollDown")
-        body.classList.add("scrollUp")
+    }
+
+    if(currentScroll > lastScroll && !body.classList.contains("veryScrollDown") && body.classList.contains("active")){
+        body.classList.remove("scrollDown")
+        body.classList.add("veryScrollDown")
+    }
+
+    if(currentScroll < lastScroll && body.classList.contains("veryScrollDown") && body.classList.contains("active")){
+        body.classList.remove("scrollDown")
+        body.classList.remove("veryScrollDown")
     }
 
     lastScroll = currentScroll;
