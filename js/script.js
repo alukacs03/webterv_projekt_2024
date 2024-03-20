5714bcc408e26cb66887375efd9d7a7f2e1adc72
BarnaSanyi <146037957+BarnaSanyi@users.noreply.github.com>
2024-03-21 00:41:36 +0100

    Felfele nyíl kész
    Amúgy a fasz kivan, hogy ez a szar nem akarja validnak elfogadni azt, hogy egy linkbe belerakunk egy gombot és ezért nekem ki kell cserélnem az összes gombot egy kurva div-re és ez a fos nem akar működni. Én ezt már nem hiszem el. Én ezt nem.. nem.. nem..
    
    Csessz meg magát minden és mindenki :)

--- js/script.js
+++ js/script.js
@@ -1,34 +1,43 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".menu");
-const body = document.querySelector("#pageHeader");
+const header = document.querySelector("#pageHeader");
+const arrow = document.querySelector("#upArrow");
+console.log(arrow);
 let lastScroll = 0;
 
 hamburger.addEventListener("click", () =>{
     hamburger.classList.toggle("active")
     navMenu.classList.toggle("active")
-    body.classList.toggle("active")
+    header.classList.toggle("active")
 })
 
 window.addEventListener('scroll', () => {
     const currentScroll = window.pageYOffset
- 
-    if(currentScroll > lastScroll && !body.classList.contains("scrollDown") && !body.classList.contains("active")){
-        body.classList.add("scrollDown")
-        
+    
+    if(currentScroll == 0) {
+        arrow.classList.add("scrollDown")
     }
 
-    if(currentScroll < lastScroll && body.classList.contains("scrollDown") && !body.classList.contains("active")){
-        body.classList.remove("scrollDown")
+    if(currentScroll > lastScroll && !header.classList.contains("scrollDown") && !header.classList.contains("active")){
+        header.classList.add("scrollDown")
+        arrow.classList.add("scrollDown")
     }
 
-    if(currentScroll > lastScroll && !body.classList.contains("veryScrollDown") && body.classList.contains("active")){
-        body.classList.remove("scrollDown")
-        body.classList.add("veryScrollDown")
+    if(currentScroll < lastScroll && header.classList.contains("scrollDown") && !header.classList.contains("active")){
+        header.classList.remove("scrollDown")
+        arrow.classList.remove("scrollDown")
     }
 
-    if(currentScroll < lastScroll && body.classList.contains("veryScrollDown") && body.classList.contains("active")){
-        body.classList.remove("scrollDown")
-        body.classList.remove("veryScrollDown")
+    if(currentScroll > lastScroll && !header.classList.contains("veryScrollDown") && header.classList.contains("active")){
+        header.classList.remove("scrollDown")
+        arrow.classList.add("scrollDown")
+        header.classList.add("veryScrollDown")
+    }
+
+    if(currentScroll < lastScroll && header.classList.contains("veryScrollDown") && header.classList.contains("active")){
+        header.classList.remove("scrollDown")
+        arrow.classList.remove("scrollDown")
+        header.classList.remove("veryScrollDown")
     }
 
     lastScroll = currentScroll;
